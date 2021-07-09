c340a2031c90f2d58f19cb10f54c12c475276e2b
Pedro Cruz <pedrocrvz@gmail.com>
2021-07-09 17:24:52 +0200

    chore: bump limit-orders-react and change fallback storage

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,6 +1,6 @@
 import { Order } from '@gelatonetwork/limit-orders-lib'
 
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 // Initialize Firebase
@@ -14,13 +14,21 @@ if (!firebase.apps.length) {
 
 const firestore = firebase.firestore()
 
-export const addOrderToDB = async (account: string, order: Order): Promise<boolean> => {
+export const addOrderToDB = async (chainId: number, account: string, order: Order): Promise<boolean> => {
   if (!firebase.apps.length || !firestore) return false
 
   if (!order.handler) order.handler = null
+
+  console.log('chainId', chainId)
+  console.log('account', account)
+  console.log('order', order)
+
   return firestore
-    .collection(account)
-    .add(order)
+    .collection('orders')
+    .doc(chainId.toString())
+    .collection(account.toLowerCase())
+    .doc(order.id.toLowerCase())
+    .set(order)
     .then(() => true)
     .catch(() => false)
 }
